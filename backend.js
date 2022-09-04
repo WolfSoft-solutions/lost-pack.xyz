a2e479d9e2700f22fcd95f4fe0df9f26a060ddf6
Commander R <wmxavier2004@gmail.com>
2022-09-04 22:47:53 +0200

    made some changes
    Added api files for another project of mine that i also added to the gitignore.
    made the api files automated in the backend, added a favicon and a 404 page

--- backend.js
+++ backend.js
@@ -1,12 +1,38 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const fs = require('fs');
 const port = 3000
 
 app.set('view engine', 'ejs')
+app.use(express.static('static'))
 
 app.get('/', (req, res) => {
     res.status(200).render('pages/home')
 })
+
+app.get('/members', (req, res) => {
+  res.status(200).render('pages/members')
+})
+
+
+const dir = './api';
+const Folders = fs. readdirSync(dir)
+
+for(const folder of Folders){
+  const apiFiles = fs.readdirSync(`./api/${folder}`).filter(file => file.endsWith(`.json`));
+  for (const file of apiFiles) {
+    const defFile = file.replace('.json', '');
+    app.get('/test/'+ folder + '/' + defFile, (req, res) => {
+      res.status(200).sendFile(__dirname + '/api/' + folder+ `/` + file);
+    })
+  }
+}
+
+
+app.get('*', (req, res) => {
+  res.status(404).render('pages/404')
+})
+
 app.listen(port, () => {
   console.log(`App listening at port ${port}`)
 })
